5f382bcac15cc0d211f59dba8c22056929917591
ambetsa <ambetsajoseph@gmail.com>
2023-03-17 05:46:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description using main function
-* this program creates with proper grammar, but the outcome is a piece of art,
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
- 	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
