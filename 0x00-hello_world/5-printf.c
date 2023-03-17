2f17d03d4f5a5d8bd2fb6aa624ea1b3a2ebd6bc0
ambetsa <ambetsajoseph@gmail.com>
2023-03-17 05:02:28 -0700

    cisfun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description using main function
+* this program creates with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+ 	return (0);
+}
